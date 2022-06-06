d9dc8e66f9318a80376a9bb6bf9cb78e8e27e3f7
ImranRH321 <imranrh247@gmail.com>
2022-06-06 20:19:07 +0600

    simple-page-and-browser-router-settup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <h2>Header ap</h2>
+      <Routes>
+        {/* <Route  path='/home' element={}></Route> */}
+       <Route path='/about' element={<}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h2>Copyright {new Date().getFullYear()}</h2>
+        </div>
+    );
+};
+
+export default Footer;
