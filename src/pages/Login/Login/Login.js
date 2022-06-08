407b78a48b44a4827f0c5cab751d01de0283c792
ImranRH321 <imranrh247@gmail.com>
2022-06-08 22:36:05 +0600

    social login initial setup

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -3,6 +3,7 @@ import { Button, Form } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
+import SocialLogin from "../SocialLogin/SocialLogin";
 
 const Login = () => {
   const emailRef = useRef("");
@@ -36,15 +37,10 @@ const Login = () => {
       <h2 className="text-primary text-center">Login Page</h2>
       <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
           <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
           <Form.Control
             ref={passwordRef}
             type="password"
@@ -59,7 +55,7 @@ const Login = () => {
         </Button>
       </Form>
       <p>
-        New user Ginius car{" "}
+        New user Ginius car
         <Link
           to="/register"
           className="text-danger pe-auto text-decoration-none"
@@ -68,6 +64,7 @@ const Login = () => {
           Register
         </Link>
       </p>
+      <SocialLogin></SocialLogin>
     </div>
   );
 };
